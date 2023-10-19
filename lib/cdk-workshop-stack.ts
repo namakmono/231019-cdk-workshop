2fe8ea4cfe59c9e7fb877aed5f98fe504dd06f4f
Ubuntu <ubuntu@ip-172-31-18-73.ap-northeast-1.compute.internal>
2023-10-19 14:44:14 +0000

    Hit Counter追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,25 +1,29 @@
 import { Stack, StackProps, App } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
     
     // define an AWS Lambda resource
     const hello = new lambda.Function(this, 'HelloHandler', {
       // execution environment
-      runtime: lambda.Runtime.NODEJS_18_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       // code loaded from "lambda" directory
       code: lambda.Code.fromAsset('lambda'),
       // file is "hello", funcion is "handler"
       handler: 'hello.handler'
     })
     
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    })
+    
     // define an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     })
 
   }
