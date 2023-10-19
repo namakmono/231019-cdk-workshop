bbb9069ee1a995f8665a9e1be4b72fd49bd0dc5f
Ubuntu <ubuntu@ip-172-31-18-73.ap-northeast-1.compute.internal>
2023-10-19 11:50:37 +0000

    API Gateway追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps, App } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
@@ -14,6 +15,11 @@ export class CdkWorkshopStack extends Stack {
       code: lambda.Code.fromAsset('lambda'),
       // file is "hello", funcion is "handler"
       handler: 'hello.handler'
+    })
+    
+    // define an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     })
 
   }
