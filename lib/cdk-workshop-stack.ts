27c13dbd69096539d6df6d714aa0fc0f99e23196
Ubuntu <ubuntu@ip-172-31-18-73.ap-northeast-1.compute.internal>
2023-10-19 06:33:08 +0000

    Hello Lambda作成

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,10 +1,20 @@
 import { Stack, StackProps, App } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    // nothing here
+    // define an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      // execution environment
+      runtime: lambda.Runtime.NODEJS_18_X,
+      // code loaded from "lambda" directory
+      code: lambda.Code.fromAsset('lambda'),
+      // file is "hello", funcion is "handler"
+      handler: 'hello.handler'
+    })
 
   }
 }
